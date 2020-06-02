f53490fe0b5b8466c7441f37c6a482c2224702af
DianaMS <dmartinezs@uni.pe>
2020-06-02 08:54:46 -0500

    actualización de ruta en window.location.hash

--- src/index.js
+++ src/index.js
@@ -10,13 +10,14 @@ const init = () => {
 window.addEventListener('load', init);
 auth.onAuthStateChanged((user) => {
   if (user) {
-    window.location.hash = '#/home';
+    window.location.hash = '#/';
     if (user.metadata.creationTime === user.metadata.lastSignInTime) {
+      // eslint-disable-next-line consistent-return
       db.collection('users').doc(user.uid).get().then((doc) => {
         if (!doc.exists) {
           return db.collection('users').doc(user.uid).set({
             bio: '',
-            posts: {}
+            posts: {},
           });
         }
       });
