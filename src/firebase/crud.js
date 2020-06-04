2ccf9df57f899fdabca452f6a8bbfedcd12a4532
yud-cumba <yudith.cumba@gmail.com>
2020-06-04 09:20:39 -0500

    delete

--- src/firebase/crud.js
+++ src/firebase/crud.js
@@ -1,11 +1,13 @@
 export const deletePost = (id, userId) => {
   db.collection('posts').doc(id).delete().then(() => {
-    // llamar a colecion de usuarios
     console.log(userId);
   })
     .catch((error) => {
       console.error('Error removing document: ', error);
     });
+  db.collection('cities').doc(userId)({
+    doc: firebase.firestore.FieldValue.delete(),
+  });
 };
 
 export const editPost = () => {
