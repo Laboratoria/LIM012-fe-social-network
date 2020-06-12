f44b0dcc8a279a60dacb8763eb45452f6ca191e3
DianaMS <dmartinezs@uni.pe>
2020-06-12 08:35:18 -0500

    ocultar y mostrar modal de opciones, y boton guardar msm

--- src/view/template-comments.js
+++ src/view/template-comments.js
@@ -1,3 +1,5 @@
+import { updateComment } from '../firebase/crud.js';
+
 export const renderComment = (postsComments, postId, sizeComments) => {
   const divComment = document.getElementById(postId);
   divComment.innerHTML = '';
@@ -15,29 +17,69 @@ export const renderComment = (postsComments, postId, sizeComments) => {
             <h2><b>${comment.userName}</b><br>${shortTime} ${shortDate}</h2>
             </div>
             <span class="comment">
-                <i id="option-id" class="fas fa-ellipsis-h"></i>
+                <i id="option-${com.id}" class="fas fa-ellipsis-h"></i>
             </span>
         </div>
         <div class= "main-comment">
             <div class="text-post">
-                <p class="p-comment" id="textComment-id}">${comment.content}</p>
-                <div class="save-comment hide" idComent="id"><i class="far fa-save"></i></div>
+                <p class="p-comment" id="textComment-${com.id}">${comment.content}</p>
+                <div class="save-comment hide" idComent="${com.id}"><i class="far fa-save"></i></div>
             </div>
-            <div class="tooltip-container hide" id="show-tootTip-id">
+            <div class="hide tooltip-container" id="show-tootTip-${com.id}">
                 <div class="tooltip">
-                    <div idComent="id" class="opt update-comment edit-id"><i class="fas fa-edit icon-tool"></i> <span>Editar</span></div>
-                    <div idComent="id" class="opt del"> <i class="fas fa-trash-alt icon-tool"></i><span>Eliminar</span></div>
+                    <div idComent="${com.id}" class="opt update-comment edit-${com.id}"><i class="fas fa-edit icon-tool"></i> <span>Editar</span></div>
+                    <div idComent="${com.id}" class="opt del"> <i class="fas fa-trash-alt icon-tool"></i><span>Eliminar</span></div>
                 </div>
             </div>
         </div>
       `;
     div.innerHTML = template;
 
-    const commentPhoto = document.querySelector('.user-comment');
-    if (comment.userPhoto) {
-      commentPhoto.src = comment.userPhoto;
+    const optionsOfComments = div.querySelector(`#option-${com.id}`);
+    if (optionsOfComments) {
+      optionsOfComments.addEventListener('click', () => {
+        const tool = document.querySelector(`#show-tootTip-${com.id}`);
+        tool.classList.toggle('hide');
+      });
     }
+    const editComment = div.querySelector(`.edit-${com.id}`);
+    if (editComment) {
+      editComment.addEventListener('click', () => {
+        const tool = document.querySelector(`#show-tootTip-${com.id}`);
+        tool.classList.toggle('hide');
+      });
+    }
+
+    const photoBeforeComment = document.querySelectorAll('.first');
+    photoBeforeComment.forEach((ele) => {
+      if (comment.userPhoto) {
+        ele.src = comment.userPhoto;
+      }
+    });
+
     divComment.appendChild(div);
+
+    const iconSaveCom = div.querySelectorAll('.save-comment');
+    console.log(iconSaveCom);
+    if (iconSaveCom.length) {
+      iconSaveCom.forEach((comments) => {
+        comments.addEventListener('click', (e) => {
+          e.preventDefault();
+          console.log('click Guardar')
+          const idComent = comments.getAttribute('idComent');
+          const newContent = document.querySelector('.p-comment');
+          if (newContent.innerText.trim() !== '') {
+            newContent.contentEditable = 'false';
+            comments.classList.add('hide');
+            const text = newContent.innerText.trim();
+            updateComment(idComent, text);
+          }
+        });
+      });
+    } else {
+      console.log('no encuentro')
+    }
+
     return div;
   });
   const sizeComment = document.getElementById(`size-${postId}`);
