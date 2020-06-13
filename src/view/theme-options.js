b563a7e45207ed9e12d3e121f6e254bfe709d117
DianaMS <dmartinezs@uni.pe>
2020-06-13 11:33:23 -0500

    eventos para los botones modo light y dark

--- src/view/theme-options.js
+++ src/view/theme-options.js
@@ -6,5 +6,17 @@ export default () => {
     <button class="light-mode">LIGHT MODE <i class="far fa-sun"></i></button>
     <button class="dark-mode">DARK MODE <i class="far fa-moon"></i></button>`;
   div.innerHTML = divcontent;
+
+  const darkMode = div.querySelector('.dark-mode');
+  const lightMode = div.querySelector('.light-mode');
+
+  darkMode.addEventListener('click', () => {
+    document.body.classList.add('dark');
+  });
+
+  lightMode.addEventListener('click', () => {
+    document.body.classList.remove('dark');
+  });
+
   return div;
 };
