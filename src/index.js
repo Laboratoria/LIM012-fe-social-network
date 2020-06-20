d0e48ce50073980a532ea7944670bdb84f9d1891
DianaMS <dmartinezs@uni.pe>
2020-06-20 16:39:57 -0500

    configuración de cambios de vista

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import { example } from './example.js';
+import { logout } from './firebase/auth.js';
+import { changeView } from './router.js';
 
-example();
+const init = () => {
+  changeView(window.location.hash);
+  window.addEventListener('hashchange', () => {
+    changeView(window.location.hash);
+  });
+};
+
+window.addEventListener('load', init);
+
+// DISPLAY MENU
+const settingsButton = document.querySelector('.fa-cog');
+const menu = document.querySelector('.menu-nav');
+settingsButton.addEventListener('click', () => {
+  menu.classList.toggle('display-flex');
+});
+// LOG OUT
+const logOutButton = document.querySelector('#log-out');
+logOutButton.addEventListener('click', () => {
+  logout();
+  window.location.hash = '#/log-in';
+});
