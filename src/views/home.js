fb99f8d079434d6481189bbeb0856f2a8f54debc
yud-cumba <yudith.cumba@gmail.com>
2020-06-20 20:56:39 -0500

    cover photo ok

--- src/views/home.js
+++ src/views/home.js
@@ -1,4 +1,7 @@
-import { firstTimeUser, getPosts } from '../firebase/firestore.js';
+import {
+  firstTimeUser, getPosts, updateDocument, getDocument,
+} from '../firebase/firestore.js';
+import { addFileToStorage, getFileFromStorage } from '../firebase/storage.js';
 
 export const home = () => {
   const div = document.createElement('div');
@@ -7,7 +10,9 @@ export const home = () => {
   const homeView = `
     <div class="user-info">
       <div class="user-cover-photo-container">
-        <div class="user-cover-photo"><img src="" alt="cover photo"></div>
+        <label for="cover-img-selected"><i class="fas fa-camera"></i></label>
+        <input type="file" id="cover-img-selected" name="cover-picture" accept="image/png, image/jpeg">
+        <img class="user-cover-photo" alt="cover photo">
         <img src="images/profile-cube.png" alt="profile photo" class="user-profile-photo pic-style">
       </div>
       <div class="username-bio">
@@ -29,7 +34,11 @@ export const home = () => {
   // Personalize Home
   const homePosts = div.querySelector('#home-posts');
   const ProfileName = div.querySelector('.username-bio h3');
+  const coverPhoto = div.querySelector('.user-cover-photo');
   firebase.auth().onAuthStateChanged((user) => {
+    getDocument('users', user.uid, (doc) => {
+      coverPhoto.src = doc.data().coverPhoto;
+    });
     if (user) {
       ProfileName.innerHTML = user.displayName;
       firstTimeUser(user.uid, user.displayName, user.photoURL);
@@ -37,10 +46,23 @@ export const home = () => {
       if (user.photoURL) {
         const photoPost = div.querySelectorAll('.pic-style');
         photoPost.forEach((imgTag) => {
+          // eslint-disable-next-line no-param-reassign
           imgTag.src = user.photoURL;
         });
       }
     }
+    const changeCover = div.querySelector('#cover-img-selected');
+    changeCover.addEventListener('change', (event) => {
+      const file = event.target.files[0];
+      const refPath = `${user.uid}/${file.name}`;
+      changeCover.name = refPath;
+      addFileToStorage(refPath, file).then((data) => {
+        getFileFromStorage(data.metadata.fullPath).then((url) => {
+          coverPhoto.src = url;
+          updateDocument('users', user.uid, 'coverPhoto', url);
+        });
+      });
+    });
   });
   return div;
 };
