b3a016db642a4a4a873f0e1ce4bc33392ffc2552
yud-cumba <yudith.cumba@gmail.com>
2020-06-22 12:05:45 -0500

    comentarios ok

--- src/views/profile.js
+++ src/views/profile.js
@@ -5,6 +5,7 @@ export const profile = () => {
   const div = document.createElement('div');
   div.id = 'profile-posts';
   // Personalize profile
+  console.log('holi estoy en profile');
   const user = currentUser();
   getPosts(user.uid, div, 'userId', user.uid);
   getComments(user.uid);
