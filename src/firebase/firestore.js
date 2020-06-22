e2fa16d7408cb6d4fce1c52ec0ad8153cea0fa7e
FiorellaChilcon <fiorellachilc_20@hotmail.com>
2020-06-22 12:47:09 -0500

    final

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line import/no-cycle
-// const db = firebase.firestore();
-
 export const getData = (callback, collectionName) => firebase.firestore().collection(collectionName)
   .onSnapshot((docs) => {
     const data = [];
@@ -10,15 +7,21 @@ export const getData = (callback, collectionName) => firebase.firestore().collec
     callback(data);
   });
 
-// eslint-disable-next-line max-len
-export const getDocument = (collectionName, docId, callback) => firebase.firestore().collection(collectionName).doc(docId)
+export const getDocument = (collectionName, docId, callback) => firebase.firestore()
+  .collection(collectionName).doc(docId)
   .get()
   .then((doc) => {
     callback(doc);
   });
 
-export const firstTimeUser = (userId, displayName, profilePhoto) => {
-  return firebase.firestore().collection('users').doc(userId).get().then((doc) => {
+export const getRealTimeDocument = (collectionName, docId, callback) => firebase.firestore()
+  .collection(collectionName).doc(docId)
+  .onSnapshot((doc) => {
+    callback(doc);
+  });
+
+export const firstTimeUser = (userId, displayName, profilePhoto) => firebase.firestore().collection('users').doc(userId).get()
+  .then((doc) => {
     if (!doc.exists) {
       firebase.firestore().collection('users').doc(userId).set({
         userName: displayName,
@@ -31,42 +34,35 @@ export const firstTimeUser = (userId, displayName, profilePhoto) => {
       });
     }
   });
-};
 
-export const addDocumentIdToUserCollection = (userId, docId, field) => {
-  return firebase.firestore().collection('users').doc(userId).update({
-    [field]: firebase.firestore.FieldValue.arrayUnion(docId),
-  });
-};
+export const addDocumentIdToUserCollection = (userId, docId, field) => firebase.firestore().collection('users').doc(userId).update({
+  [field]: firebase.firestore.FieldValue.arrayUnion(docId),
+});
 
-export const addPost = (userId, content, photo, visibility) => {
-  return firebase.firestore().collection('posts').add({
-    userId,
-    content,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    photo,
-    visibility,
-    likes: 0,
-  });
-};
-export const addComment = (userId, postId, content) => {
-  return firebase.firestore().collection('comments').add({
-    userId,
-    postId,
-    content,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-  });
-};
-export const updateDocument = (collection, docId, field, value) => {
-  return firebase.firestore().collection(collection).doc(docId).update({
+export const addPost = (userId, content, photo, visibility) => firebase.firestore().collection('posts').add({
+  userId,
+  content,
+  timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+  photo,
+  visibility,
+  likes: 0,
+});
+
+export const addComment = (userId, postId, content) => firebase.firestore().collection('comments').add({
+  userId,
+  postId,
+  content,
+  timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+});
+
+export const updateDocument = (collection, docId, field, value) => firebase.firestore()
+  .collection(collection).doc(docId).update({
     [field]: value,
   });
-};
-// eslint-disable-next-line max-len
-export const deleteDocument = (collection, docId) => firebase.firestore().collection(collection).doc(docId).delete();
 
-export const deleteDocumentIdFromUserCollection = (userId, docId, field) => {
-  return firebase.firestore().collection('users').doc(userId).update({
-    [field]: firebase.firestore.FieldValue.arrayRemove(docId),
-  });
-};
+export const deleteDocument = (collection, docId) => firebase.firestore()
+  .collection(collection).doc(docId).delete();
+
+export const deleteDocumentIdFromUserCollection = (userId, docId, field) => firebase.firestore().collection('users').doc(userId).update({
+  [field]: firebase.firestore.FieldValue.arrayRemove(docId),
+});
